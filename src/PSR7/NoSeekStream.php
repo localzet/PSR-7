ab96af838c019c21a98d25ad2b7a408f6f949010
Иван Зорин <local.zet@yandex.ru>
2022-07-28 15:09:06 +0500

    Initial commit

---
+++ src/PSR7/NoSeekStream.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * @package     PSR Http Message
+ * @link        https://localzet.gitbook.io
+ * 
+ * @author      localzet <creator@localzet.ru>
+ * 
+ * @copyright   Copyright (c) 2018-2020 Zorin Projects 
+ * @copyright   Copyright (c) 2020-2022 NONA Team
+ * 
+ * @license     https://www.localzet.ru/license GNU GPLv3 License
+ */
+
+namespace localzet\PSR\PSR7;
+
+use Psr\Http\Message\StreamInterface;
+
+/**
+ * Stream decorator that prevents a stream from being seeked
+ */
+class NoSeekStream implements StreamInterface
+{
+    use StreamDecoratorTrait;
+
+    public function seek($offset, $whence = SEEK_SET)
+    {
+        throw new \RuntimeException('Cannot seek a NoSeekStream');
+    }
+
+    public function isSeekable()
+    {
+        return false;
+    }
+}
